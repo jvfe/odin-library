4002a43ca278dac31007589a1b499fab81a3fb79
jvfe <jvfecav@gmail.com>
2021-02-16 10:50:23 -0300

    feat: Add basic logic for creating book cards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const libraryView = document.querySelector(".library");
+const addBookButton = document.querySelector("#add-book");
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -5,10 +9,54 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
+Book.prototype.createElement = function () {
+  const bookElement = document.createElement("div");
+  bookElement.classList.add("book-card");
+  bookElement.dataset.bookIndex = myLibrary.indexOf(this);
+
+  const title = document.createElement("h4");
+  title.textContent = this.title;
+  const author = document.createElement("h6");
+  author.textContent = `by ${this.author}`;
+  const pages = document.createElement("p");
+  pages.textContent = `${this.pages}pg`;
+  const read = document.createElement("p");
+  read.textContent = this.read;
+  const removeBook = document.createElement("button");
+  removeBook.textContent = "Remove";
+  removeBook.classList.add("remove-book");
+
+  const bookInfo = [title, author, pages, read, removeBook];
+  bookInfo.forEach((info) => {
+    info.classList.add("book-info");
+    bookElement.appendChild(info);
+  });
+
+  return bookElement;
 };
 
-let hobbit = new Book("The Hobbit", "JRR Tolkien", "295", "read");
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
 
-console.log(hobbit.info());
+let hobbit = new Book("The Hobbit", "JRR Tolkien", "295", "read");
+let b = new Book("The Hobbit", "JRR Tolkien", "295", "read");
+let c = new Book("The Hobbit", "JRR Tolkien", "295", "read");
+addBookToLibrary(hobbit);
+addBookToLibrary(b);
+addBookToLibrary(c);
+
+myLibrary.forEach((book) => {
+  libraryView.appendChild(book.createElement());
+});
+
+const removeBookButtons = document.querySelectorAll(".remove-book");
+
+removeBookButtons.forEach((removeButton) => {
+  removeButton.addEventListener("click", () => {
+    let bookToRemove = removeButton.parentElement;
+    let bookIndex = bookToRemove.dataset.bookIndex;
+    myLibrary.splice(bookIndex, 1);
+    bookToRemove.remove();
+  });
+});
